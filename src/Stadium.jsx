d9c5728c871552df77d4eb658c073d44213bc3c0
lazy-sky <1cielo@naver.com>
2021-10-15 00:05:40 +0900

    green: 경기장 목록(이름) 렌더링

--- src/Stadium.jsx
+++ src/Stadium.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 export default function Stadium() {
+  const stadiums = [
+    { id: 1, name: 'Porte de la Chapelle Arena', sportsList: ['배드민턴', '리듬 체조'] },
+    { id: 2, name: 'Grand Palais', sportsList: ['펜싱', '태권도'] },
+    { id: 3, name: 'La Concorde', sportsList: ['BMX 프리스타일', '스케이트보드', '브레이킹 댄스', '3대3 농구'] },
+    { id: 4, name: 'Eiffel Tower Stadium', sportsList: ['비치 발리볼'] },
+  ];
+
   return (
     <div>
-      <h3>경기장</h3>
+      <h3>경기장 목록</h3>
+      <ul>
+        {stadiums.map((stadium) => (
+          <li key={stadium.id}>
+            {stadium.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
