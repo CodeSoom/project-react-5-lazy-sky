b0c024aacba1dd7354fc72fd13f1d15afefcb50a
lazy-sky <1cielo@naver.com>
2021-10-14 23:59:17 +0900

    red: 경기장 이름 렌더링 테스트

--- src/Stadium.test.jsx
+++ src/Stadium.test.jsx
@@ -8,11 +8,17 @@ import React from 'react';
 import Stadium from './Stadium';
 
 describe('Stadium', () => {
+  const stadiums = [
+    { name: 'Porte de la Chapelle Arena', sportsList: ['배드민턴', '리듬 체조'] },
+    { name: 'Eiffel Tower Stadium', sportsList: ['비치 발리볼'] },
+  ];
+
   it('Stadium', () => {
     const { getByText } = render((
       <Stadium />
     ));
 
-    expect(getByText('경기장')).not.toBeNull();
+    expect(getByText('Porte de la Chapelle Arena')).not.toBeNull();
+    expect(getByText('Eiffel Tower Stadium')).not.toBeNull();
   });
 });
